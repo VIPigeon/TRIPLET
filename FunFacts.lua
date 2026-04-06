065939fa05b4b160fabd474e17f8dfa26a055ff9
VIPigeon <slv.bta@gmail.com>
2026-04-07 00:44:56 +0300

    визуальные и мелкие изменения

---
+++ FunFacts.lua
@@ -0,0 +1,9 @@
+
+-- у каждого животного будет своё имя. так мне будет легче ориентироваться
+FunFacts = {
+    lama_rainbow = "It looks a bit lika a pinata. Please don't hit it",
+    hippopotamus = "Often confused with a river-horse",
+    river_horse = "Often confused with a hippopotamus",
+    peacock = "The peacock’s tail spread can reach up to 9 pixels",
+    cros = "The wingspan of a crow can reach up to about 1 meter",
+}
